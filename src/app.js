84fd08e96b1f2e6e338b3b3b07b7c06c06148d83
polina <polina.zdanovich.02@mail.ru>
2023-01-03 11:21:36 +0300

    project

--- src/app.js
+++ src/app.js
@@ -1,5 +1,12 @@
 const express = require("express")
+const bodyParser = require("body-Parser")
 
 const app = express()
+
+app.use(bodyParser.json())
+
+app.use((error, req, res, next) => {
+    res.status(500).send(error.message)
+})
 
 module.exports = app
