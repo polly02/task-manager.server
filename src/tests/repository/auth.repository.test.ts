ef06ae25230555544efd2540d3e27302ec27eab2
polina <polina.zdanovich.02@mail.ru>
2023-01-29 15:33:38 +0300

    test auth repository

---
+++ src/tests/repository/auth.repository.test.ts
@@ -0,0 +1,33 @@
+import { getUserByEmailDB, createUserDB } from "../../repository/auth.repository"
+
+const mClient = {
+    query: jest.fn()
+}
+
+jest.mock("pg", () => {
+    const mPool = {
+        connect: jest.fn(() => mClient)
+    }
+    return { Pool: jest.fn(() => mPool) }
+})
+
+describe("getUserByEmailDB function", () => {
+    test("should return succes", async () => {
+        const mockTask = [{ id: 1, name: "name", surname: "surname", pwd: "pwd12345", email: "email@mail.ru", status: 1 }]
+        mClient.query.mockResolvedValue({ rows: mockTask })
+        const expected = await getUserByEmailDB("email@mail.ru")
+
+        expect(expected).toEqual(mockTask)
+    })
+})
+
+describe("createUserDB", ()=>{
+    test("should return succes", async()=>{
+        const mockUsers = [{ id: 1, name: "name", surname: "surname", pwd: "pwd12345", email: "email@mail.ru", status: 1 }]
+
+        mClient.query.mockResolvedValue({ rows: mockUsers })
+        await createUserDB("name", "surname", "pwd12345", "email@mail.ru")
+
+        expect(mClient.query).toBeCalledWith('COMMIT')
+    })
+})
